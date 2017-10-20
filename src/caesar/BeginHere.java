2b3900e4b2acd0a48eea3acd51c3c13f1acd2880
compsci <compsci@compsci>
2017-10-20 10:45:15 -0600

    Working except for the z wrap

--- src/caesar/BeginHere.java
+++ src/caesar/BeginHere.java
@@ -1,9 +1,24 @@
 package caesar;
 
+import java.util.Scanner;
+
 public class BeginHere {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner kbd = new Scanner(System.in);
+		
+		
+		CaesarCipher myCipher = new CaesarCipher();
+		
+		System.out.println("Please enter the plain text to encode.");
+		String plainText = kbd.nextLine();
+		
+		System.out.println("Please enter the key: ");
+		int key = kbd.nextInt();
+		System.out.println("Your key is: " + key);
+		
+		String codedText = myCipher.encode(plainText, key);
+		System.out.println("Your secret message is: " + codedText);
 
 	}
 
--- src/caesar/CaesarCipher.java
+++ src/caesar/CaesarCipher.java
@@ -1,5 +1,52 @@
 package caesar;
 
 public class CaesarCipher {
+	
+	private String alpha = "abcdefghijklmnopqrstuvwxyz";
+
+	public String encode(String plainText, int key) {
+		
+		String secretText="";
+		int index, keyAdded;
+		
+		System.out.println("test, alpha length = " + alpha.length());
+		System.out.println("test, first char is: " + alpha.charAt(0));
+		System.out.println("test, last char is: " + alpha.charAt(alpha.length()-1));
+		
+		for(int i = 0; i < alpha.length(); i++) {
+			System.out.println(alpha.charAt(i));
+		}
+		System.out.println("\n\n");
+		
+		for (int i = 0; i < plainText.length(); i++) {
+			
+			System.out.println(plainText.charAt(i));
+		}
+		
+		/*
+		 * each char that I read from plainText I find the index in alpha
+		 * for example, read A look up index for A get 0
+		 * 				read M look up index for M get 12
+		 * then, add keey to that index, so A index 0 become 0 + key,
+		 * and 0+key becomes my coded char
+		 * let's say key = 1, then A index 0, 0 + 1 = 1, charAt 1 is B
+		 * and that is my encoding
+		 */
+		
+		for (int i=0; i<plainText.length(); i++) {
+			
+			index = alpha.indexOf(plainText.charAt(i));
+			//System.out.println("index = " + index);
+			
+			keyAdded = index + key;
+			
+			System.out.println("encoded is " + alpha.charAt(keyAdded));
+			
+			
+		}
+		
+		return secretText;
+		
+	}
 
 }
