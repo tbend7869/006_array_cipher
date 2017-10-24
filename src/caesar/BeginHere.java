ef0940df5e86737e922cbd010cd691062707ec49
compsci <compsci@compsci>
2017-10-24 10:08:45 -0600

    This ACTUAllY fixes the letter case

--- src/caesar/BeginHere.java
+++ src/caesar/BeginHere.java
@@ -11,7 +11,7 @@ public class BeginHere {
 		CaesarCipher myCipher = new CaesarCipher();
 		
 		System.out.println("Please enter the plain text to encode.");
-		String plainText = kbd.nextLine();
+		String plainText = kbd.nextLine().toLowerCase();
 		
 		System.out.println("Please enter the key: ");
 		int key = kbd.nextInt();
